cd06562d0bec289d860a3a66316a3f84c15f61a2
jaylu <jaylu1995@outlook.com>
2024-06-17 16:42:01 +0800

    更新0.8.0,更新依赖，新增部分特性

--- pytestifyx/__init__.py
+++ pytestifyx/__init__.py
@@ -3,6 +3,6 @@ from .utils.logs.core import log
 
 __author__ = "luyh"
 
-__version__ = "0.7.3.2"
+__version__ = "0.8.0"
 
 __description__ = "pytestifyx is a pytest-based automation testing framework for api, ui, app testing"
